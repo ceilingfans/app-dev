ed2758f1af4ce21af041d7e77f0bf3bd9483ce95
Amurmir <mdameer769@gmail.com>
2023-11-14 14:29:29 +0800

    feat: flask boiler plate

--- website/main.py
+++ website/main.py
@@ -1,1 +1,10 @@
-print("hello from website")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/<name>')
+def index(name):
+    return render_template('home.html',name = name)
+
+if __name__ == "__main__":
+    app.run()
