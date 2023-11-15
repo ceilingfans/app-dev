3f858e1b017e341cc8d3e8d009dd11b7a023c2d8
Nekomine <110485055+Wuddke@users.noreply.github.com>
2023-11-15 14:28:04 +0800

    feat: made get database functions into classes
    made a class to set a db and db collection and functions to insert into the db

--- api/getdatabase.py
+++ api/getdatabase.py
@@ -1,6 +1,5 @@
 from pymongo import MongoClient
 from pymongo.errors import ConnectionFailure
-from pymongo import MongoClient
 from dotenv import load_dotenv, find_dotenv
 import os
 
@@ -8,30 +7,29 @@ load_dotenv(find_dotenv())
 
 password = os.environ.get("MONGODB_PWD")
 
-CONNECTION_STRING=f"YOUR_MONGO_STRING + {password}"
+CONNECTION_STRING=f"CONNECTION_STRING + {password}"
 
-def get_userdatabase():
-    
-    MONGO_URL = CONNECTION_STRING
-    
-    try:
-        mongo_client = MongoClient(MONGO_URL)
-        mongo_client.server_info()
-    except ConnectionFailure:
-        print("Invalid Mongo DB URL. Please Check Your Credentials! Exiting...")
-        quit(1)
-    print("Connection Successful")
-    
-    return mongo_client['user_list']
-#Add second DB.
-def get_inventorydatabase():
-    return
+class Database:
+    def __init__(self,dbname: str):
+        MONGO_URL = CONNECTION_STRING
+        try:
+            mongo_client = MongoClient(MONGO_URL)
+            mongo_client.server_info()
+            self.client = mongo_client
+        except ConnectionFailure:
+            print("Invalid Mongo DB URL. Please Check Your Credentials! Exiting...")
+            quit(1)
+        print("Connection Successful")
+        self.dbname = dbname
 
-if __name__ == "__main__":   
-   
-   userdb = get_userdatabase()
-#  invdb = get_inventorydatabase()
-
+class DatabaseCollection(Database):
+    def __init__(self,dbname,dbcol):
+        super().__init__(dbname)
+        self.dbcol = dbcol
+    def get_Database(self):
+        db = self.client[self.dbname]
+        collection = db[f"{self.dbcol}"]
+        return collection
 
 
 
