f2937bc69d7abf36771302238d01401afd052ca9
awned <65061212+ceilingfans@users.noreply.github.com>
2024-01-09 00:11:04 +0800

    test: billing crud

--- website/main.py
+++ website/main.py
@@ -1,9 +1,12 @@
 import sys
+import uuid
+
 from flask import Flask, render_template, redirect , request
 from pymongo import MongoClient
 #set up actually good PYTHONPATH
 #sys.path.insert(1, "G://app-dev//app-dev")
 from api.structures.User import User
+from api.structures.billinghistory import Billinghistory
 from api.db.driver import Driver
 db = Driver()
 app = Flask(__name__)
@@ -17,6 +20,55 @@ def index():
 def page():
     return render_template("contact.html")
 
+
+@app.route("/billing_crud", methods=["GET", "POST"])
+def billing_crud():
+    html = "billing_crud.html"
+
+    if request.method == "POST":
+        match request.form["submit"]:
+            case "create_bill":
+                bill = Billinghistory({
+                    "customerid": request.form["customer_id"],
+                    "billid": str(uuid.uuid4()),
+                    "status": False
+                })
+                db.create_bill(bill)
+                return render_template(html, created_bill=str(dict(bill)))
+
+            case "find_bill":
+                bill_id = request.form["bill_id"]
+                result = db.get_bill_by_id(bill_id)
+                if result is None:
+                    return render_template(html, found_bill=f"Bill with id {bill_id} not found")
+
+                return render_template(html, found_bill=str(dict(result)))
+
+            case "update_bill":
+                bill_id = request.form.get("bill_id")
+                customer_id = request.form.get("customer_id")
+                status = request.form.get("bill_status") == "payed"
+
+                bill = Billinghistory({
+                    "billid": bill_id,
+                    "customerid": customer_id,
+                    "status": status
+                })
+
+                result = db.update_bill(bill)
+                return render_template(html, updated_bill=str(result))
+
+            case "delete_bill":
+                bill_id = request.form.get("bill_id")
+
+                result = db.delete_bill_by_id(bill_id)
+                return render_template(html, deleted_bill=f"Deleted bill with id {bill_id}" if result else f"No bill with id {bill_id} found")
+
+            case _:
+                return "<body>invalid submit button pressed<br>somehow pressed button that doesnt exist</body>"
+
+    else:
+        return render_template(html)
 
 @app.route("/CRUDTEST",methods=['GET','POST'])
 def crudtest():
